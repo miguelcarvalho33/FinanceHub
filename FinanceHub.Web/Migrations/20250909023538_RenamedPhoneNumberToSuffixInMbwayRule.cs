80cf6c067f42cc8a53419d25023f5a1d458aa1fd
Miguel Carvalho <miguel.carvalho.33@hotmail.com>
2025-09-09 12:28:10 +0100

    Renomeia coluna PhoneNumber para PhoneNumberSuffix
    Esta atualização renomeia a coluna "PhoneNumber" para "PhoneNumberSuffix" na tabela "MbwayRules" através de uma migração. O modelo de dados foi ajustado para incluir a nova propriedade "PhoneNumberSuffix" na entidade "MbwayRule". O arquivo de snapshot do contexto do banco de dados foi atualizado para a versão "9.0.8". Além disso, houve modificações nos arquivos binários do banco de dados, refletindo as alterações na estrutura.

---
+++ FinanceHub.Web/Migrations/20250909023538_RenamedPhoneNumberToSuffixInMbwayRule.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceHub.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedPhoneNumberToSuffixInMbwayRule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "MbwayRules",
+                newName: "PhoneNumberSuffix");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumberSuffix",
+                table: "MbwayRules",
+                newName: "PhoneNumber");
+        }
+    }
+}
